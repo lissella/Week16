0201b935553319253704119513e1ca255d395910
lissella <69310460+lissella@users.noreply.github.com>
2021-03-02 12:41:09 +0700

    Add files via upload

---
+++ Function2.js
@@ -0,0 +1,99 @@
+let msg = prompt("Please enter year of birth: ");
+let zodiac = parseInt(zodiac);
+Function myZodiac(year,ily){
+    const name = {
+        name1 = 'Rat',
+        name2 = 'Oxen',
+        name3 = 'Tiger',
+        name4 = 'Rabbit',
+        name5 = 'Dragon',
+        name6 = 'Shake',
+        name7 = 'House',
+        name8 = 'Sheep',
+        name9 = 'Monkey',
+        name10 = 'Roostep',
+        name11 = 'Dog',
+        name12 = 'Plg',
+    }
+    switch(year){
+        case 2020: case 2018: case 1996: case 1984: case 1972:
+            alert(`${year} is $(name,name1) Rat ZOCIAC SIGNS ${ily}`);
+            console.log(`${year}  is $(name,name1) Rat ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`${year} is $(name,name1) Rat ZOCIAC SIGNS ${ily}`);
+            break;
+        case 2021: case 2019: case 1997: case 1985: case 1973:
+            alert(`${year} is $(name,name2) Oxen ZOCIAC SIGNS ${ily}`);
+            console.log(`${year} is $(name,name2) Oxen ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`${year} is $(name,name2) Oxen ZOCIAC SIGNS ${ily}`);
+            break;
+        case 2022: case 2020: case 1998: case 1986: case 1974:
+            alert(`${year} is (name,name3) Tiger ZOCIAC SIGNS ${ily}`);
+            console.log(`${year} is (name,name3) Tiger ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`${year} is (name,name3) Tiger ZOCIAC SIGNS ${ily}`);
+            break;
+        case 2023: case 2021: case 1999: case 1987: case 1975:
+            alert(`${year} is (name,name4) Rabbit ZOCIAC SIGNS ${ily}`);
+            console.log(`${year} is (name,name4) Rabbit ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`year is (name,name4) Rabbit ZOCIAC SIGNS ${ily}`);
+            break;
+        case 2024: case 2022: case 2000: case 1988: case 1976:
+            alert(`${year} is (name,name5) Dragon ZOCIAC SIGNS ${ily}`);
+            console.log(`${year} is (name,name5) Dragon ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`${year} is (name,name5) Dragon ZOCIAC SIGNS ${ily}`);
+            break;
+        case 2025: case 2023: case 2001: case 1989: case 1977:
+            alert(`${year} is (name,name6) Snake ZOCIAC SIGNS ${ily}`);
+            console.log(`${year} is (name,name6) Snake ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`${year} is (name,name6) Snake ZOCIAC SIGNS ${ily}`);
+            break;
+        case 2026: case 2024: case 2002: case 1990: case 1978:
+            alert(`${year} is (name,name7) House ZOCIAC SIGNS ${ily}`);
+            console.log(`${year} (name,name7) is House ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`${year} is (name,name7) House ZOCIAC SIGNS ${ily}`);
+            break;
+        case 2027: case 2025: case 2003: case 1991: case 1979:
+            alert(`${year} is (name,name8) Sheep ZOCIAC SIGNS ${ily}`);
+            console.log(`${year} is (name,name8) Sheep ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`${year} is S(name,name8) heep ZOCIAC SIGNS ${ily}`);
+            break;
+        case 2028: case 2026: case 2004: case 1992: case 1980:
+            alert(`${year} is (name,name9) Monkey ZOCIAC SIGNS ${ily}`);
+            console.log(`${year} is (name,name9) Monkey ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`${year} is (name,name9) Monkey ZOCIAC SIGNS ${ily}`);
+            break;
+        case 2029: case 2027: case 2005: case 1993: case 1981:
+            alert(`${year} +"is (name,name10)Rooster ZOCIAC SIGNS ${ily}`);
+            console.log(`${year} is (name,name10) Rooster ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`${year} is (name,name10) Rooster ZOCIAC SIGNS ${ily}`);
+            break;
+        case 2030: case 2028: case 2006: case 1994: case 1982:
+            alert(`${year} is (name,name11) Dog ZOCIAC SIGNS ${ily}`);
+            console.log(`${year} is (name,name11) Dog ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`${year} is (name,name11) Dog ZOCIAC SIGNS ${ily}`);
+            break;
+        case 2031: case 2029: case 2007: case 1995: case 1983:
+            alert(`${year} is (name,name12) Plg ZOCIAC SIGNS ${ily}`);
+            console.log(`${year} is (name,name12) Plg ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`${year} is (name,name12) Plg ZOCIAC SIGNS ${ily}`);
+            break;
+        default:
+            alert(`year is ERROR ZOCIAC SIGNS ${ily}`);
+            console.log(`year is ERROR ZOCIAC SIGNS ${ily}`);
+            document.getElementById("hContent").innerHTML= (`year is ERROR ZOCIAC SIGNS ${ily}`);
+    }
+}
+//Function in javascript(ES6)
+//1.Declaration Function
+let data1 = prompt("Enter any value: ");
+let data2 = prompt("Enter any value: ");
+let cdata1,cdata = parseInt(data1);
+let cdata2 = parseInt(data2);
+function myZodiac(a,b)//formal parameter
+{
+    let z = a + b ;
+    console.log(`${Z} ${a} + ${b} is ${ily}`);
+    document.getElementById("hContent").innerHTML=(`${Z} ${a} + ${b} is ${ily}`);
+}
+
+myZodiac(cdata1,cdata2);"I Love You"//argument parameter(Active parameter)
+
---
+++ script.js
@@ -0,0 +1,68 @@
+let msg = prompt("Please enter year of birth: ");
+let zodiac = parseInt(zodiac);
+switch(year){
+    case 2020: case 2018: case 1996: case 1984: case 1972:
+        alert(`${year} is Rat ZOCIAC SIGNS`);
+        console.log(`${year} is Rat ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`${year} is Rat ZOCIAC SIGNS`);
+        break;
+    case 2021: case 2019: case 1997: case 1985: case 1973:
+        alert(`${year} is Oxen ZOCIAC SIGNS`);
+        console.log(`${year} is Oxen ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`${year} is Oxen ZOCIAC SIGNS`);
+    break;
+    case 2022: case 2020: case 1998: case 1986: case 1974:
+        alert(`${year} is Tiger ZOCIAC SIGNS`);
+        console.log(`${year} is Tiger ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`${year} is Tiger ZOCIAC SIGNS`);
+    break;
+    case 2023: case 2021: case 1999: case 1987: case 1975:
+        alert(`${year} is Rabbit ZOCIAC SIGNS`);
+        console.log(`${year} is Rabbit ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`year is Rabbit ZOCIAC SIGNS`);
+    break;
+    case 2024: case 2022: case 2000: case 1988: case 1976:
+        alert(`${year} is Dragon ZOCIAC SIGNS`);
+        console.log(`${year} is Dragon ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`${year} is Dragon ZOCIAC SIGNS`);
+    break;
+    case 2025: case 2023: case 2001: case 1989: case 1977:
+        alert(`${year} is Snake ZOCIAC SIGNS`);
+        console.log(`${year} is Snake ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`${year} is Snake ZOCIAC SIGNS`);
+    break;
+    case 2026: case 2024: case 2002: case 1990: case 1978:
+        alert(`${year} is House ZOCIAC SIGNS`);
+        console.log(`${year} is House ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`${year} is House ZOCIAC SIGNS`);
+    break;
+    case 2027: case 2025: case 2003: case 1991: case 1979:
+        alert(`${year} is Sheep ZOCIAC SIGNS`);
+        console.log(`${year} is Sheep ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`${year} is Sheep ZOCIAC SIGNS`);
+    break;
+    case 2028: case 2026: case 2004: case 1992: case 1980:
+        alert(`${year} is Monkey ZOCIAC SIGNS`);
+        console.log(`${year} is Monkey ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`${year} is Monkey ZOCIAC SIGNS`);
+    break;
+    case 2029: case 2027: case 2005: case 1993: case 1981:
+        alert(`${year} +"is Rooster ZOCIAC SIGNS`);
+        console.log(`${year} +"is Rooster ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`${year} is Rooster ZOCIAC SIGNS`);
+    break;
+    case 2030: case 2028: case 2006: case 1994: case 1982:
+        alert(`${year} is Dog ZOCIAC SIGNS`);
+        console.log(`${year} is Dog ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`${year} is Dog ZOCIAC SIGNS`);
+        break;
+    case 2031: case 2029: case 2007: case 1995: case 1983:
+        alert(`${year} is Plg ZOCIAC SIGNS`);
+        console.log(`${year} is Plg ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`${year} is Plg ZOCIAC SIGNS`);
+        break;
+    default:
+        alert(`year is ERROR ZOCIAC SIGNS`);
+        console.log(`year is ERROR ZOCIAC SIGNS`);
+        document.getElementById("hContent").innerHTML= (`year is ERROR ZOCIAC SIGNS`);
+}
